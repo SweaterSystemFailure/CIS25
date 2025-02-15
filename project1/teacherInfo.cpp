b5424cbb59b7b5b2b73b4a09fb6dedb0017f57cb
Ben <benjamin.s.fagan@gmail.com>
2025-02-14 17:14:12 -0800

    Added data validators and user check.

--- project1/teacherInfo.cpp
+++ project1/teacherInfo.cpp
@@ -1,18 +1,54 @@
 #include <iostream>
+#include <string>
 #include <cstring>
 #include "teacherInfo.h"
 
 using namespace std;
 
 string teacherInfo() {
-	string honorific, teacherLast, teacherFull, result;
+	string honorific, teacherLast, teacherFull;
+	char choice;
+	while (true) {
+		cout << "What honorific do your students address you by? [Ms., Mrs., Mr., Dr., etc] ";
+		getline(cin, honorific);
+		if (honorific.empty()) {
+			cout << "Invalid input. Can't be empty. Try again: " << endl;
+		}
+		else {
+			break;
+		}
+	}
 
-	cout << "What honorific do your students address you by? [Ms., Mrs., Mr., Dr., etc] ";
-	cin >> honorific;
-	cout << "What is your last name? ";
-	cin >> teacherLast;
+	while (true) {
+		cout << "What is your last name? ";
+		getline(cin, teacherLast);
+		if (teacherLast.empty()) {
+			cout << "Invalid input. Can't be empty. Try again: " << endl;
+		}
+		else {
+			break;
+		}
+	}
+
 	teacherFull = honorific + " " + teacherLast;
 	cout << "Hi " << teacherFull << "!" << endl;
+	
+	while (true) {
+		cout << "Does this look right to you? " << endl;
+		cin >> choice;
+		choice = tolower(choice);
+
+		if (choice == 'y') {
+			break;
+		}
+		else if (choice == 'n') {
+			cout << "That's okay. Let's try again." << endl;
+			teacherInfo();
+		}
+		else {
+			cout << "Invalid input. Please enter y or n." << endl;
+		}
+	}
 
 	return teacherFull;
 }
