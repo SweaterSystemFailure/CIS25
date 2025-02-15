c20396a0c1e8ffb8a3a734c3f68410bb6dad9cd9
Ben <benjamin.s.fagan@gmail.com>
2025-02-15 12:32:12 -0800

    Added a logic check so that a report can't be generated with incomplete information.

--- project1/studentReport.cpp
+++ project1/studentReport.cpp
@@ -3,31 +3,38 @@
 #include "studentReport.h"
 #include "globalVariables.h"
 #include "scorer.h"
+#include "mainMenu.h"
 
 using namespace std;
 
 void studentReport() {
-
-	cout << setfill('*') << setw(50) << "*" << endl;
-	cout << setfill('*') << setw(50) << "*" << endl;
-	cout << endl;
-	cout << setw(40) << left << "Student: " << studentInfo[0] << endl;
-	cout << setw(40) << left << "Grade: " << gradeLev << endl;
-	cout << setw(40) << left << "Age: " << studentInfo[3] << endl;
-	cout << setw(40) << left << "Student ID: " << studentInfo[1] << endl;
-	cout << setw(40) << left << "Seat: " << studentInfo[2] << endl;
-	cout << setfill('*') << setw(50) << "*" << endl;
-	cout << setw(40) << left << "Overall Score: " << setprecision(2) << scorer() << endl;
-	cout << setw(40) << left << "Overall Grade: " << letterGrade<< endl;
-	cout << setfill('*') << setw(50) << "*" << endl;
-	cout << endl;
-
-	for (int i = 0; i < assignmentTotalSize; i++) {
-		cout << "Assignment " << i + 1 << ", " << assignmentNames[i] << ": " << assignmentPointsScored[i] << "out of " << assignmentPointsPossible[i] << "." << endl;
+	if (studentInfo[0] == "undefined" || assignmentNames[0] == "undefined" || assignmentPointsPossible[0] == 0) {
+		cout << "Please be sure to populate student information and assignment information before generating a student report." << endl;
+		mainMenu();
 	}
+	else {
+		cout << setfill('*') << setw(50) << "*" << endl;
+		cout << setfill('*') << setw(50) << "*" << endl;
+		cout << endl;
+		cout << setw(40) << left << "Student: " << studentInfo[0] << endl;
+		cout << setw(40) << left << "Grade: " << gradeLev << endl;
+		cout << setw(40) << left << "Age: " << studentInfo[3] << endl;
+		cout << setw(40) << left << "Student ID: " << studentInfo[1] << endl;
+		cout << setw(40) << left << "Seat: " << studentInfo[2] << endl;
+		cout << setfill('*') << setw(50) << "*" << endl;
+		cout << setw(40) << left << "Overall Score: " << setprecision(2) << scorer() << endl;
+		cout << setw(40) << left << "Overall Grade: " << letterGrade << endl;
+		cout << setfill('*') << setw(50) << "*" << endl;
+		cout << endl;
 
-	cout << endl;
-	cout << setfill('*') << setw(50) << "*" << endl;
-	cout << setfill('*') << setw(50) << "*" << endl;
-	cout << endl;
+		for (int i = 0; i < assignmentTotalSize; i++) {
+			cout << "Assignment " << i + 1 << ", " << assignmentNames[i] << ": " << assignmentPointsScored[i] << "out of " << assignmentPointsPossible[i] << "." << endl;
+		}
+
+		cout << endl;
+		cout << setfill('*') << setw(50) << "*" << endl;
+		cout << setfill('*') << setw(50) << "*" << endl;
+		cout << endl;
+	}
+	mainMenu();
 }
